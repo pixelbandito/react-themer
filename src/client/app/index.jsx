7cdac0e329730080a1b7ff90d84a6241a72f6e9b
Chris Garcia <pixelbandito@gmil.com>
2017-08-07 23:44:19 -0500

    More cleanup

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { render } from 'react-dom';
 
 import SassJsComponent from './SassJsComponent';
 // import bootstrap from 'bootstrap.css';
@@ -8,6 +7,7 @@ require('../submodules/bootstrap/dist/css/bootstrap.css');
 require('./main.scss');
 require('sass.js');
 
+
 const App = () => (
   <div className="container pxb-u-m_t--md pxb-u-m_b--md">
     <h1>Hello World!</h1>
@@ -15,4 +15,4 @@ const App = () => (
   </div>
 );
 
-render(<App />, document.getElementById('app'));
+export default App;
