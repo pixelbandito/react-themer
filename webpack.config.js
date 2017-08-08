3ef860154d763708ab6969d84b984f7e1d62ee10
Chris Garcia <pixelbandito@gmil.com>
2017-08-07 23:44:19 -0500

    More cleanup

--- webpack.config.js
+++ webpack.config.js
@@ -1,50 +1,51 @@
-var webpack = require('webpack');
-var path = require('path');
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-var BUILD_DIR = path.resolve(__dirname, 'src/client/public');
-var APP_DIR = path.resolve(__dirname, 'src/client/app');
-var BS_DIR = path.resolve(__dirname, 'src/client/submodules/bootstrap');
+const BUILD_DIR = path.resolve(__dirname, 'dist/');
+const APP_DIR = path.resolve(__dirname, 'src/client/app');
+const BS_DIR = path.resolve(__dirname, 'src/client/submodules/bootstrap');
 
-var config = {
-    entry: APP_DIR + '/index.jsx',
-    output: {
-        path: BUILD_DIR,
-        filename: 'bundle.js'
-    },
-    module: {
-        loaders: [
-            {
-                test: /\.js$/,
-                include: APP_DIR,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.jsx?$/,
-                include: APP_DIR,
-                loader: 'babel-loader'
-            },
-            {
-                test: /\.css$/,
-                include: path.resolve(BS_DIR, 'dist/css/'),
-                loader: 'style-loader!css-loader'
-            },
-            {
-                test: /\.scss$/,
-                include: APP_DIR,
-                exclude: path.resolve(APP_DIR, 'submodules/'),
-                loader: 'style-loader!css-loader!sass-loader'
-            }
-        ]
-    },
-    node: {
-        fs: "empty"
-    },
-    resolve: {
-        extensions: ['.js', '.jsx', '.json']
-    }
-    // resolveLoader: {
-    //     root: path.resolve(__dirname, 'node_modules')
-    // }
+const config = {
+  entry: `${APP_DIR}/index.jsx`,
+  output: {
+    path: BUILD_DIR,
+    filename: 'bundle.js',
+  },
+  plugins: [new HtmlWebpackPlugin()],
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.jsx?$/,
+        include: APP_DIR,
+        loader: 'babel-loader',
+      },
+      {
+        test: /\.css$/,
+        include: path.resolve(BS_DIR, 'dist/css/'),
+        loader: 'style-loader!css-loader',
+      },
+      {
+        test: /\.scss$/,
+        include: APP_DIR,
+        exclude: path.resolve(APP_DIR, 'submodules/'),
+        loader: 'style-loader!css-loader!sass-loader',
+      },
+    ],
+  },
+  node: {
+    fs: 'empty',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx', '.json'],
+  },
+  // resolveLoader: {
+  //     root: path.resolve(__dirname, 'node_modules')
+  // }
 };
 
 module.exports = config;
