69404872b990f16af8e165d8b7515035f741335f
Chris Garcia <pixelbandito@gmil.com>
2017-08-07 23:26:55 -0500

    Cleaned up files for eslint rules

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,24 +1,18 @@
 import React from 'react';
-import {render} from 'react-dom';
+import { render } from 'react-dom';
 
-import SassJsComponent from './SassJsComponent.jsx';
+import SassJsComponent from './SassJsComponent';
 // import bootstrap from 'bootstrap.css';
 
 require('../submodules/bootstrap/dist/css/bootstrap.css');
 require('./main.scss');
 require('sass.js');
 
-class App extends React.Component {
-    render () {
-        return (
-            <div className="container pxb-u-m_t--md pxb-u-m_b--md">
-                <h1>Hello World!</h1>
-                <SassJsComponent/>
-            </div>
-        );
-    }
-}
+const App = () => (
+  <div className="container pxb-u-m_t--md pxb-u-m_b--md">
+    <h1>Hello World!</h1>
+    <SassJsComponent />
+  </div>
+);
 
-render (<App/>, document.getElementById('app'));
-
-console.log('Hello World!');
+render(<App />, document.getElementById('app'));
