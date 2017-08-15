0227c6a4b997550d5568e02210e1870f096f4ed1
Chris Garcia <pixelbandito@gmil.com>
2017-08-14 21:49:12 -0500

    add redux

--- src/client/app/index.jsx
+++ src/client/app/index.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
 
+
+import { currentTheme, themeForm, setCurrentTheme } from './themeConfig';
 import SassJsComponent from './SassJsComponent';
 // import bootstrap from 'bootstrap.css';
 
@@ -8,6 +13,18 @@ require('../submodules/bootstrap/dist/css/bootstrap.css');
 require('./main.scss');
 require('sass.js');
 
+const middleware = [thunk];
+
+const store = createStore(
+  combineReducers({
+    currentTheme,
+    themeForm,
+  }),
+  /* eslint-disable no-underscore-dangle */
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  /* eslint-enable no-underscore-dangle */
+  applyMiddleware(...middleware),
+);
 
 const App = () => (
   <div className="container pxb-u-m_t--md pxb-u-m_b--md">
@@ -16,6 +33,13 @@ const App = () => (
   </div>
 );
 
-render(<App />, document.getElementById('app'));
+render(
+  (
+    <Provider store={store}>
+      <App />
+    </Provider>
+  ),
+  document.getElementById('app'),
+);
 
 export default App;
